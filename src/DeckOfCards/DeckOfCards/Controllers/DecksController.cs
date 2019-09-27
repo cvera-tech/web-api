829f2515561b39d36a7a9cb8883fb61b1c8df1ef
jbvera <johnbenedictv@gmail.com>
2019-09-27 13:12:16 -0400

    Add CreateDeck

---
+++ src/DeckOfCards/DeckOfCards/Controllers/DecksController.cs
@@ -0,0 +1,31 @@
+﻿using DeckOfCards.Data;
+using DeckOfCards.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DeckOfCards.Controllers
+{
+    [RoutePrefix("api/decks")]
+    public class DecksController : ApiController
+    {
+        private IDeckRepository repository;
+
+        public DecksController(IDeckRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<ShortDeckInfo> Post(DeckCreate model)
+        {
+            int count = model.Count.HasValue ? model.Count.Value : 1;
+            var deck = await repository.CreateNewShuffledDeckAsync(count);
+            var deckInfo = new ShortDeckInfo()
+            {
+                DeckId = deck.DeckId,
+                Remaining = deck.Cards.Where(card => !card.Drawn).Count()
+            };
+            return deckInfo;
+        }
+    }
+}
---
+++ src/DeckOfCards/DeckOfCards/Models/DeckCreate.cs
@@ -0,0 +1,12 @@
+﻿namespace DeckOfCards.Models
+{
+    public class DeckCreate
+    {
+        public int? Count { get; set; }
+
+        public DeckCreate()
+        {
+            Count = 1;
+        }
+    }
+}
