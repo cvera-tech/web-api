cca7158b5be2eacfa24ceb9a49d16f41f89aac4c
jbvera <johnbenedictv@gmail.com>
2019-09-27 12:46:51 -0400

    Add DeckRepository

--- src/DeckOfCards/DeckOfCards/App_Start/UnityConfig.cs
+++ src/DeckOfCards/DeckOfCards/App_Start/UnityConfig.cs
@@ -1,3 +1,4 @@
+using DeckOfCards.Data;
 using System.Web.Http;
 using Unity;
 using Unity.WebApi;
@@ -9,11 +10,12 @@ namespace DeckOfCards
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
             
             // e.g. container.RegisterType<ITestService, TestService>();
+            container.RegisterType<IDeckRepository, DeckRepository>();
             
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
